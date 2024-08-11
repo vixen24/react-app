e8878b03cd5ed8badd536242fdda8ca0293d74dc
William Idakwo <30729966+vixen24@users.noreply.github.com>
2024-08-11 16:39:07 +0100

    Create index.js

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import ExampleComponent from "./components/ExampleComponent";
+
+const root = ReactDOM.createRoot(document.getElementById("container"))
+root.render(<ExampleComponent/>);
