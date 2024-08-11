14bc3adaa1b8612c01aba2b56d5aade480d46b95
William Idakwo <30729966+vixen24@users.noreply.github.com>
2024-08-11 16:40:38 +0100

    Create ExampleComponent.js

---
+++ src/client/components/ExampleComponent.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../styles/main.css"
+
+export default function ExampleComponent() {
+	return <h1>Congratulations, you have successfully deployed the application!</h1>;
+}
